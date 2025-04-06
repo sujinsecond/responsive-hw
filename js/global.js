beb819c6bc0a69b44dd5c33cbaf9e8f79ebc4469
sujin2 <sunnydayiscoming@gmail.com>
2025-04-06 22:16:25 +0900

    0406/ongoing

--- js/global.js
+++ js/global.js
@@ -1,17 +1,17 @@
-const Theme = () =>{
-    const button = document.querySelector('.btn-theme');
-  
-    if(!button) return;
-  
-    const handleTheme = () => {
-      const currentScheme = document.documentElement.style.getPropertyValue('color-scheme');
-      if (currentScheme.includes('dark')) {
-        document.documentElement.style.setProperty('color-scheme', 'light');
-      } else {
-        document.documentElement.style.setProperty('color-scheme', 'dark');
-      }
+const Theme = () => {
+  const button = document.querySelector('.btn-theme');
+
+  if (!button) return;
+
+  const handleTheme = () => {
+    const currentScheme = document.documentElement.style.getPropertyValue('color-scheme');
+    if (currentScheme.includes('dark')) {
+      document.documentElement.style.setProperty('color-scheme', 'light');
+    } else {
+      document.documentElement.style.setProperty('color-scheme', 'dark');
     }
-    
-    button.addEventListener('click', handleTheme);
-  }
-  Theme();
+  };
+
+  button.addEventListener('click', handleTheme);
+};
+Theme();
